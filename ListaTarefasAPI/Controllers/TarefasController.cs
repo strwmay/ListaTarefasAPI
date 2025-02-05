3565a76e15fe2e918dcfc01585d0e98461a07881
May <senaidev.may@gmail.com>
2025-02-05 09:21:04 -0300

    inclusão do método concluir

---
+++ ListaTarefasAPI/Controllers/TarefasController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ListaTarefasAPI.Data;
+using ListaTarefasAPI.Models;
+
+namespace ListaTarefasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        private readonly ListaTarefasContext _context;
+
+        public TarefasController(ListaTarefasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tarefas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tarefa>>> GetTarefas()
+        {
+            return await _context.Tarefas.ToListAsync();
+        }
+
+        // GET: api/Tarefas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tarefa>> GetTarefa(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return tarefa;
+        }
+
+        [HttpGet("concluidas")]
+        public async Task<ActionResult<IEnumerable<Tarefa>>> GetTarefasConcluidas()
+        {
+            return await _context.Tarefas.Where(t => t.Concluida == true).ToListAsync();
+        }
+
+        // PUT: api/Tarefas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTarefa(int id, Tarefa tarefa)
+        {
+            if (id != tarefa.TarefaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tarefa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TarefaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tarefas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Tarefa>> PostTarefa(Tarefa tarefa)
+        {
+            _context.Tarefas.Add(tarefa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTarefa", new { id = tarefa.TarefaId }, tarefa);
+        }
+
+        // DELETE: api/Tarefas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTarefa(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tarefas.Remove(tarefa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // PATCH: api/Tarefas/5/concluir - COPILOT
+        [HttpPatch("{id}/concluir")]
+        public async Task<IActionResult> ConcluirTarefa(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            tarefa.Concluida = true;
+            _context.Entry(tarefa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TarefaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool TarefaExists(int id)
+        {
+            return _context.Tarefas.Any(e => e.TarefaId == id);
+        }
+    }
+}
