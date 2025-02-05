1d2e112fb1f522d79098ad9d8884dac8e6433339
May <senaidev.may@gmail.com>
2025-02-05 10:32:30 -0300

    inclusão do CORS

--- ListaTarefasAPI/Program.cs
+++ ListaTarefasAPI/Program.cs
@@ -3,6 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Adicionar e Configurar o CORS (Cross-Origin Resource Sharing) para permitir que a API seja acessada por qualquer origem.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
 // Add services to the container.
 
 // Adicionar a string de conexao ao container.
